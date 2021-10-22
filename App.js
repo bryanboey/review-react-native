584478452b0d5c919e31c6402e9f4b335568523f
bryanboey <bryanboey@gmail.com>
2021-10-22 09:37:41 +0800

    removed unused imports

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import RootStack from './routes/RootStack';
-import Home from './screens/Home';
 
 const store = createStore(
   rootReducer,
